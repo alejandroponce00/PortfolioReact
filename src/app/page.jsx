cb7fe734a7cc49c6baf2893753a938555c26c0bc
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2023-11-03 18:15:25 -0300

    cambnio3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,15 @@ export default function Home() {
         
         <p className="text-secondary text-center">¡Hola, soy  desarrollador fullstack con experiencia en la creación de aplicaciones web de alto rendimiento! Mi enfoque se centra en la construcción de soluciones web atractivas y eficientes utilizando una variedad de tecnologías de vanguardia. Mi objetivo es combinar diseño y funcionalidad para ofrecer experiencias digitales excepcionales.<br></br>
 
-
+       
+       <ul><h2>Proyectos</h2>
+        <li>
+        <a href="https://alejandroponce00.github.io/PORTFOLIO/#">Portfolio HTML</a>
+        </li>
+        <li>
+        <a href="https://alejandroponce00.github.io/ArteSanaMosaicos/">ArteSana Mosaicos</a>
+        </li>
+       </ul>
 
 </p> 
          <ParticlesBackground /> 
