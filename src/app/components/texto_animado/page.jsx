82455fd23483983ba7c4ee4e463cac27c51c7f81
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2023-11-04 17:31:21 -0300

    commit6

--- src/app/components/texto_animado/page.jsx
+++ src/app/components/texto_animado/page.jsx
@@ -6,7 +6,7 @@ function Deletrea() {
   return (
     <div>
       <Slide>
-        <h2 className='text-white bg-dark text-center'>React Awesome Reveal</h2>
+        
       </Slide>
       <Fade delay={300} cascade damping={0.1} className='text-secondary'>
         Alejandro Ponce
