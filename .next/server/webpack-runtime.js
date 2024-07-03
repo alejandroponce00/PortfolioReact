f38ac8be08cb9d5cfac247b51804f471c4a292de
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-07-03 16:05:00 -0300

    cambio color fondo

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("70d4dbdaaf5b0f33")
+/******/ 		__webpack_require__.h = () => ("92847386f7025e8a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
